0195647fa55a7d0613066e1153df5ef47fcf7302
Maksim Ivanyukhin <chelovechek.maks@mail.ru>
2024-06-03 10:05:09 +0300

    Trying format svg

--- src/components/AppFilter.vue
+++ src/components/AppFilter.vue
@@ -47,11 +47,6 @@ const selectedRarity = ref(props.properties.rarity);
             <label class="filter__game-label" :for="`filter-game-${game.id}`">
               <SvgTemplate
                 class="filter__game-icon"
-                :icon-id="game.id"
-                :sprite="spritePath"
-              />
-              <SvgTemplate
-                class="filter__game-icon"
                 icon-id="test"
                 :sprite="spritePath"
               />
